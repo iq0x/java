a4e02584fbcd64eefd7fa2064424258fa63f58ce
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-16 11:01:56 +0200

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
-public class HelloWorld
+public class hello
 {
 
        public static void main (String[] args)
        {
-             System.out.println("Hello World!");
+             System.out.println("hello");
        }
 }
