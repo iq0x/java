151e25f7b3491840479bf852edb5f30ed7436fc3
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-16 16:15:08 +0200

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,6 +3,11 @@ public class Main
        public static void main (String[] args)
        {
              System.out.println("hello man");
+              /*
+              mega comment
+              */
+              
+              //comment
        }
 }
 
