76f65b3346bdaab6f9f462d3b93741bdfc1ba0e4
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-16 15:49:07 +0200

    Create input.java

---
+++ input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+      System.out.println("input:");
+      Scanner scannerVariable = new Scanner(System.in);
+      System.out.println("your input is: " + scannerVariable.nextLine());
+    }
+}
