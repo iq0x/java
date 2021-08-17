4aaf982f651657b97cbedb1c3f52c45610d8df40
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-17 13:24:56 +0200

    Update variable.java

--- variable.java
+++ variable.java
@@ -5,9 +5,9 @@ public class Main
               boolean a = false;      //8 bit 	true/false
               byte b = 2;             //8 bit 	-27 bis 27-1
               short c = 34;           //16 bit 	-215 bis 215-1
-              char d = test;          //16 bit 	0 bis 65535
+              char d = 'A';          //16 bit 	0 bis 65535
               int e = 3;              //32 bit 	-231 bis 231-1
-              float f = 1.223;        //32 bit 	+/-1,4E-45 bis +/-3,4E+38
+              float f = 1.00f;        //32 bit 	+/-1,4E-45 bis +/-3,4E+38
               long g = 3123;          //64 bit 	-263 bis 263-1
               double h = 34.42;       //64 bit 	+/-4,9E-324 bis +/-1,7E+308
               System.out.println(a);
