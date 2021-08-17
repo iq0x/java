50f82263aa80324f2cb8a6ecf406137c56648d81
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-18 00:15:38 +0200

    Update slotmaschine_v1.java

--- slotmaschine_v1.java
+++ slotmaschine_v1.java
@@ -11,34 +11,34 @@ public class slotmaschine
 
     while(i<n)
     {
-    i++;
-    Random rand = new Random();
+      i++;
+      Random rand = new Random();
 
-    int field1 = rand.nextInt(3);
-    int field2 = rand.nextInt(3);
-    int field3 = rand.nextInt(3);
+      int field1 = rand.nextInt(3);
+      int field2 = rand.nextInt(3);
+      int field3 = rand.nextInt(3);
 
-    System.out.println(field1 + " " + field2 + " " + field2);
+      System.out.println(field1 + " " + field2 + " " + field2);
 
-    if (field1 == 1 && field2 == 1 && field3 == 1)
-    {
-      System.out.println("you win 100 Dollar!");
-    }
+      if (field1 == 1 && field2 == 1 && field3 == 1)
+      {
+        System.out.println("you win 100 Dollar!");
+      }
 
-    else if (field1 == 2 && field2 == 2 && field3 == 2)
-    {
-      System.out.println("you win 200 Dollar!");
-    }
+      else if (field1 == 2 && field2 == 2 && field3 == 2)
+      {
+        System.out.println("you win 200 Dollar!");
+      }
 
-    else if (field1 == 0 && field2 == 0 && field3 == 0)
-    {
-      System.out.println("you win 50 Dollar!");
-    }
+      else if (field1 == 0 && field2 == 0 && field3 == 0)
+      {
+        System.out.println("you win 50 Dollar!");
+      }
 
-    else
-    {
-      System.out.println("looser!");
-    }
+      else
+      {
+        System.out.println("looser!");
+      }
     }
   }
 }
