5a1a6e292dfc68e8bef367834f931580bcd61657
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-17 13:41:13 +0200

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,13 @@
+public class arithmetic
+{
+       public static void main (String[] args)
+       {
+              double a = 0.0;
+              double b = 0.0;
+
+              a = 5.0;
+
+              System.out.println("a = " + a);
+              System.out.println("b = " + b);
+       }
+}
