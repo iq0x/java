4dbaea144cf70b93036779e4e9b03301bd9b3414
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-21 03:06:59 +0200

    Add files via upload

---
+++ bool.java
@@ -0,0 +1,15 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+public class Main
+{
+      int a = 0, b = 90;
+      a = (b != 75) ? 70 : 80;
+      if(b > 90) a = 90;
+      if(b < 80 && b >= 10 || a == 90)
+        a = 100;
+      if(b == 110 && a != b - 40)
+        a = 110;
+      System.out.println(a);
+}
