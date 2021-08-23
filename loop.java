d5638f222184cc34f59ab605ca77e1084b4915a0
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-23 10:24:32 +0200

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,66 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+		System.out.println("loop 1");
+		int i1 = 3;
+		int n1 = 20;
+		
+		while(i1 < n1)
+		{
+				System.out.println(i1);
+				i1 = i1 + 4;
+		}
+		
+		
+		
+		System.out.println("loop 2");
+		int i2 = 8;
+		int n2 = 257;
+		
+		while(i2 < n2)
+		{
+				System.out.println(i2);
+				i2 = i2 * 2;
+		}
+		
+		
+		
+		System.out.println("loop 3");
+		int i3 = 243;
+		int n3 = 2;
+		
+		while(i3 > n3)
+		{
+				System.out.println(i3);
+				i3 = i3 / 3;
+		}
+		
+		
+		
+		System.out.println("loop 4");
+		int i4 = 3;
+		int n4 = -4;
+		
+		while(i4 > n4)
+		{
+				System.out.println(i4);
+				i4--;
+		}
+		
+		
+		
+		System.out.println("loop 5");
+		
+		int i5 = 0;
+		
+		while(i5 < 2)
+		{
+				System.out.print("1");
+				System.out.print("0");
+				i5++;
+	
+		}
+		System.out.print("1");
+    }
+}
