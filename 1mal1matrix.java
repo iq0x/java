2dafbf1d3abf71a86103a82f273ae5ceaabead77
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-23 14:34:30 +0200

    Update 1mal1matrix.java

--- 1mal1matrix.java
+++ 1mal1matrix.java
@@ -3,15 +3,12 @@ public class Main
     public static void main(String[] args)
     {
 		int x = 1;
-		int result;
-		
-		
+	
 		while(x <= 10)
 		{
 				int y = 1;
 				while(y <= 10)
 				{
-					result = x * y;
 					System.out.print(y*x+"\t");
 					y++;
 
@@ -22,3 +19,4 @@ public class Main
 		}
     }
 }
+
