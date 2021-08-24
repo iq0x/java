53bc846443e98636e7cc44ea98840e6c4a0f6cfd
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-24 10:03:43 +0200

    Add files via upload

---
+++ loop_do.java
@@ -0,0 +1,14 @@
+public class Main
+{
+       public static void main (String[] args)
+       {
+		   int x = 0;
+		   
+            do
+            {
+				x++;
+				System.out.println(x);
+			}while(x < 5);
+       } 
+}
+
