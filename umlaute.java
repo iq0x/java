1c54c02d7a7bbac5e8cace655f8e658680fb8ec7
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-24 15:14:44 +0200

    Update umlaute.java

--- umlaute.java
+++ umlaute.java
@@ -5,17 +5,25 @@ public class Main
 			String text = "tÄsöööööööööööööüöäüöäüöüöääöüüäööäüööäüöäüüöäüöäüöäüöäüöäüäöüäüäüäüöäüäöüöäüöüöäüöäööööö";
             
             int count = 0;
-            int umlaute = 0;
+            int umlauteA = 0;
+            int umlauteU = 0;
+            int umlauteO = 0;
             
             while (count < text.length())
             {
                   char G = text.charAt(count);
                   System.out.print(G);
                   count++;
-                  if (G == 'Ä' || G == 'ä' || G == 'Ö' || G == 'ö' || G == 'Ü'|| G == 'ü')
-					umlaute++;
+                  if (G == 'Ä' || G == 'ä')
+					umlauteA++;
+				  if (G == 'ü' || G == 'Ü')
+					umlauteU++;
+				   if (G == 'Ö' || G == 'ö')
+					umlauteO++;
             }
             System.out.println();
-            System.out.println("umlaute: "+ umlaute);
+            System.out.println("umlauteA: "+ umlauteA);
+            System.out.println("umlauteU: "+ umlauteU);
+            System.out.println("umlauteO: "+ umlauteO);
        }
 }
