d3ba89ab2930ba411bc8de0519398c0c1162adbb
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-26 20:26:12 +0200

    Update bindez.java

--- bindez.java
+++ bindez.java
@@ -8,8 +8,8 @@ public class Main
 		int dez = 0;
 		
 		Scanner blub = new Scanner(System.in);
-		System.out.print("zahl -b für binär\nzahl -d für dezimal\n");
-		System.out.print("give me the shit: \n--------------------\n");
+		System.out.print("-b\tbinär\n-d\tdezimal\n-help\thilfe\n");
+		System.out.print("--------------------\ngive me the shit: \n--------------------\n");
 
 		String zahl = blub.nextLine();		
 		
@@ -35,12 +35,15 @@ public class Main
 				System.out.print("binär: ");
 				while(input!=0)
 				{
-					if(input % 2 == 0) 
-						{input /= 2; System.out.print(0);}
-					else if(input % 2 == 1) 
-						{input /= 2; System.out.print(1);}	
+					if(input % 2 == 0) {input /= 2; System.out.print(0);}
+					else if(input % 2 == 1) {input /= 2; System.out.print(1);}	
 				}
 				System.out.println();
+			}
+			
+			if(zahl.contains("-help"))
+			{
+				System.out.println("lol dir ist nicht zu helfen xDDD");
 			}
 		}
 		else
