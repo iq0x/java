142aa80598e91bc4e34cbbde3b75962d0ac4ee66
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-26 22:21:03 +0200

    Update bindez.java

--- bindez.java
+++ bindez.java
@@ -8,37 +8,53 @@ public class Main
 		int dez = 0;
 		
 		Scanner blub = new Scanner(System.in);
-		System.out.print("-b\tbinär\n-d\tdezimal\n-help\thilfe\n");
+		System.out.print("zahl-b\tbinär\nzahl-d\tdezimal\n-help\thilfe\n");
 		System.out.print("--------------------\ngive me the shit: \n--------------------\n");
 
 		String zahl = blub.nextLine();		
 		
 		if(!zahl.isEmpty())
 		{
-			if(zahl.contains("-b"))
-			{
-				zahl = zahl.replace("-b", ""); 
-				Integer input = Integer.valueOf(zahl);
-				while(input != 0)
-				{
-					dez += ((input % 10)) * Math.pow(2,count);
-					input /= 10;  
-					count++;
-				}
-				System.out.println("dezimal: " + dez);
-			}
-			
 			if(zahl.contains("-d"))
 			{
 				zahl = zahl.replace("-d", ""); 
-				Integer input = Integer.valueOf(zahl);
-				System.out.print("binär: ");
-				while(input!=0)
+				if (zahl.matches("[0-1]+"))
 				{
-					if(input % 2 == 0) {input /= 2; System.out.print(0);}
-					else if(input % 2 == 1) {input /= 2; System.out.print(1);}	
+					Integer input = Integer.valueOf(zahl);
+					while(input != 0)
+					{
+						dez += ((input % 10)) * Math.pow(2,count);
+						input /= 10;  
+						count++;
+					}
+					System.out.println("dezimal: " + dez);
+					
 				}
-				System.out.println();
+				else
+				{
+					System.out.println("das ist keine binärzahl!");
+				}
+			}
+			
+			if(zahl.contains("-b"))
+			{
+				
+				zahl = zahl.replace("-b", ""); 
+				if (zahl.matches("[0-1]+"))
+				{
+					Integer input = Integer.valueOf(zahl);
+					System.out.print("binär: ");
+					while(input!=0)
+					{
+						if(input % 2 == 0) {input /= 2; System.out.print(0);}
+						else if(input % 2 == 1) {input /= 2; System.out.print(1);}	
+					}
+					System.out.println();
+				}
+				else
+				{
+					System.out.println("das ist keine dezimalzahl!");
+				}
 			}
 			
 			if(zahl.contains("-help"))
@@ -50,4 +66,5 @@ public class Main
 			System.out.println("unbekannter befehl");
 	}
 }
+
 
