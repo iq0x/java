f10806fa677287d868976699c0169b57077cd06b
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-27 17:06:35 +0200

    Update array_suche.java

--- array_suche.java
+++ array_suche.java
@@ -1,21 +1,14 @@
-import java.util.Arrays;
-
 public class Main
 {
        public static void main (String[] args)
        {
-		
-			String[] fields = new String[] { "sergio","oliver","emre","dario","misterx"};
-			String suchen = "misterx";
-
-			for(int i = 0; i < fields.length; i++)
-			{
-				if(fields[i].equals(suchen))
-				{
-				
-					System.out.println("hab dich");
-				}
-			}
 			
+			String[] fields = new String[] { "sergio","oliver","emre","dario","misterx"};
+			String suchen = "sergio";
+
+			for (int i = 0; fields[i].equals(suchen) || fields.length == 0; i++)
+			{
+				System.out.println("hab dich");	
+			}
        }
 }
