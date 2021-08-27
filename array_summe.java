3a3d17a9117c87fa66523aa1e7981c47762412a5
iq0x <88105363+iq0x@users.noreply.github.com>
2021-08-27 17:11:32 +0200

    Update array_summe.java

--- array_summe.java
+++ array_summe.java
@@ -3,13 +3,11 @@ public class Main
        public static void main (String[] args)
        {
           int zahlen[] = {5,10,2,3};
-          int summe = 0;
-          
+          int summe = 0;       
           for(int i:zahlen) 
           {
               summe += i;
-          }
-          
+          } 
           System.out.println("summe :"+ summe);
        }
 }
