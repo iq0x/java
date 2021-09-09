8fd939d9d0c444325628c3b08c16a1b2b7c4eb5c
iq0x <88105363+iq0x@users.noreply.github.com>
2021-09-09 11:26:47 +0200

    Add files via upload

---
+++ hangman_animation.java
@@ -0,0 +1,184 @@
+import java.io.*;
+
+
+public class Main 
+{  
+    public static void main (String args[])
+    {   
+		System.out.print("\033[H\033[2J"); 
+		System.out.flush(); 
+			
+		int i = 0;
+		int wait = 800; 
+		while ( i < 1 )
+		{
+			i++;
+			
+			System.out.println("┣━━━━>                  ┏━━━━━━━━━━━━┓    <━━━━━┫");
+			System.out.println("┣━━━━━>    ╔═══════╗    ┃   help!    ┃     <━━━━┫");
+			System.out.println("┣━━━━>     ║ O  o  ║    ┃   me!!     ┃    <━━━━━┫");
+			System.out.println("┣━━━━━>    ║       ║    ┗━━━━━━━━━━━━┛     <━━━━┫");
+			System.out.println("┣━━━━>     ║━━━━   ║                      <━━━━━┫");
+			System.out.println("┣━━━━━>    ╚═══╦═══╝                       <━━━━┫");
+			System.out.println("┣━━━━>     ╔═══╩═══╗                      <━━━━━┫");
+			System.out.println("┣━━━━━> ║  ║       ║  ║                    <━━━━┫");
+			System.out.println("┣━━━━> ═╬══╣       ╠══╬═                  <━━━━━┫");
+			System.out.println("┣━━━━━> ║  ║       ║  ║                    <━━━━┫");
+			System.out.println("┣━━━━>     ╚═══╦═══╝                      <━━━━━┫");
+			System.out.println("┣━━━━━>     ╔══╩══╗                        <━━━━┫");
+			System.out.println("┣━━━━>      ║     ║                       <━━━━━┫");
+			System.out.println("┣━━━━━>   ══╝     ╚══                      <━━━━┫");
+			System.out.println("═════════════════════════════════════════════════");
+			
+		
+			try{Thread.sleep(wait);}catch(InterruptedException e){System.out.println(e);}
+			clrscr();
+			
+			
+			System.out.println("┣━━━━━>                 ┏━━━━━━━━━━━━┓   <━━━━┫");
+			System.out.println("┣━━━━>     ╔═══════╗    ┃   help!    ┃  <━━━━━┫");
+			System.out.println("┣━━━━━>    ║ o  O  ║    ┃   me!!     ┃   <━━━━┫");
+			System.out.println("┣━━━━>     ║       ║    ┗━━━━━━━━━━━━┛  <━━━━━┫");
+			System.out.println("┣━━━━━>    ║━━━━   ║                     <━━━━┫");
+			System.out.println("┣━━━━>     ╚═══╦═══╝                    <━━━━━┫");
+			System.out.println("┣━━━━━>    ╔═══╩═══╗                     <━━━━┫");
+			System.out.println("┣━━━━>  ║  ║       ║  ║                 <━━━━━┫");
+			System.out.println("┣━━━━━ ═╬══╣       ╠══╬═                 <━━━━┫");
+			System.out.println("┣━━━━>  ║  ║       ║  ║                 <━━━━━┫");
+			System.out.println("┣━━━━━>    ╚═══╦═══╝                     <━━━━┫");
+			System.out.println("┣━━━━>      ╔══╩══╗                     <━━━━━┫");
+			System.out.println("┣━━━━━>     ║     ║                      <━━━━┫");
+			System.out.println("┣━━━━>    ══╝     ╚══                   <━━━━━┫");
+			System.out.println("═════════════════════════════════════════════════");
+			
+			try{Thread.sleep(wait);}catch(InterruptedException e){System.out.println(e);}
+			
+			clrscr();
+			
+			
+			System.out.println("┣━━━━>                               <━━━━━┫");
+			System.out.println("┣━━━━━>    ╔═══════╗                  <━━━━┫");
+			System.out.println("┣━━━━>     ║ O  o  ║                 <━━━━━┫");
+			System.out.println("┣━━━━━>    ║       ║                  <━━━━┫");
+			System.out.println("┣━━━━>     ║━━━━   ║                 <━━━━━┫");
+			System.out.println("┣━━━━━>    ╚═══╦═══╝                  <━━━━┫");
+			System.out.println("┣━━━━>     ╔═══╩═══╗                 <━━━━━┫");
+			System.out.println("┣━━━━━> ║  ║       ║  ║               <━━━━┫");
+			System.out.println("┣━━━━> ═╬══╣       ╠══╬═             <━━━━━┫");
+			System.out.println("┣━━━━━> ║  ║       ║  ║               <━━━━┫");
+			System.out.println("┣━━━━>     ╚═══╦═══╝                 <━━━━━┫");
+			System.out.println("┣━━━━━>     ╔══╩══╗                   <━━━━┫");
+			System.out.println("┣━━━━>      ║     ║                  <━━━━━┫");
+			System.out.println("┣━━━━━>   ══╝     ╚══                 <━━━━┫");
+			System.out.println("═════════════════════════════════════════════════");
+			
+			try{Thread.sleep(wait);}catch(InterruptedException e){System.out.println(e);}
+			
+			clrscr(); 
+			
+			System.out.println("┣━━━━━>                            <━━━━┫");
+			System.out.println("┣━━━━>     ╔═══════╗              <━━━━━┫");
+			System.out.println("┣━━━━━>    ║ o  O  ║               <━━━━┫");
+			System.out.println("┣━━━━>     ║       ║              <━━━━━┫");
+			System.out.println("┣━━━━━>    ║━━━━   ║               <━━━━┫");
+			System.out.println("┣━━━━>     ╚═══╦═══╝              <━━━━━┫");
+			System.out.println("┣━━━━━>    ╔═══╩═══╗               <━━━━┫");
+			System.out.println("┣━━━━>  ║  ║       ║  ║           <━━━━━┫");
+			System.out.println("┣━━━━━>═╬══╣       ╠══╬═           <━━━━┫");
+			System.out.println("┣━━━━>  ║  ║       ║  ║           <━━━━━┫");
+			System.out.println("┣━━━━━>    ╚═══╦═══╝               <━━━━┫");
+			System.out.println("┣━━━━>      ╔══╩══╗               <━━━━━┫");
+			System.out.println("┣━━━━━>     ║     ║                <━━━━┫");
+			System.out.println("┣━━━━>    ══╝     ╚══             <━━━━━┫");
+			System.out.println("═════════════════════════════════════════════════");
+			
+			try{Thread.sleep(wait);}catch(InterruptedException e){System.out.println(e);}
+			
+			clrscr();
+			
+			
+			System.out.println("┣━━━━>                         <━━━━━┫");
+			System.out.println("┣━━━━━>    ╔═══════╗            <━━━━┫");
+			System.out.println("┣━━━━>     ║ O  o  ║           <━━━━━┫");
+			System.out.println("┣━━━━━>    ║       ║            <━━━━┫");
+			System.out.println("┣━━━━>     ║━━━━   ║           <━━━━━┫");
+			System.out.println("┣━━━━━>    ╚═══╦═══╝            <━━━━┫");
+			System.out.println("┣━━━━>     ╔═══╩═══╗           <━━━━━┫");
+			System.out.println("┣━━━━━> ║  ║       ║  ║         <━━━━┫");
+			System.out.println("┣━━━━> ═╬══╣       ╠══╬═       <━━━━━┫");
+			System.out.println("┣━━━━━> ║  ║       ║  ║         <━━━━┫");
+			System.out.println("┣━━━━>     ╚═══╦═══╝           <━━━━━┫");
+			System.out.println("┣━━━━━>     ╔══╩══╗             <━━━━┫");
+			System.out.println("┣━━━━>      ║     ║            <━━━━━┫");
+			System.out.println("┣━━━━━>   ══╝     ╚══           <━━━━┫");
+			System.out.println("═════════════════════════════════════════════════");
+			
+			try{Thread.sleep(wait);}catch(InterruptedException e){System.out.println(e);}
+			
+			clrscr();
+			
+			System.out.println("┣━━━━━>                      <━━━━┫");
+			System.out.println("┣━━━━>     ╔═══════╗        <━━━━━┫");
+			System.out.println("┣━━━━━>    ║ o  O  ║         <━━━━┫");
+			System.out.println("┣━━━━>     ║       ║        <━━━━━┫");
+			System.out.println("┣━━━━━>    ║━━━━   ║         <━━━━┫");
+			System.out.println("┣━━━━>     ╚═══╦═══╝        <━━━━━┫");
+			System.out.println("┣━━━━━>    ╔═══╩═══╗         <━━━━┫");
+			System.out.println("┣━━━━>  ║  ║       ║  ║     <━━━━━┫");
+			System.out.println("┣━━━━━>═╬══╣       ╠══╬═     <━━━━┫");
+			System.out.println("┣━━━━>  ║  ║       ║  ║     <━━━━━┫");
+			System.out.println("┣━━━━━>    ╚═══╦═══╝         <━━━━┫");
+			System.out.println("┣━━━━>      ╔══╩══╗         <━━━━━┫");
+			System.out.println("┣━━━━━>     ║     ║          <━━━━┫");
+			System.out.println("┣━━━━>    ══╝     ╚══       <━━━━━┫");
+			System.out.println("═════════════════════════════════════════════════");			
+			try{Thread.sleep(wait);}catch(InterruptedException e){System.out.println(e);}
+			
+			clrscr();
+			
+			System.out.println("┣━━━━>                   <━━━━━┫");
+			System.out.println("┣━━━━━>    ╔═══════╗      <━━━━┫");
+			System.out.println("┣━━━━>     ║ O  o  ║     <━━━━━┫");
+			System.out.println("┣━━━━━>    ║       ║      <━━━━┫");
+			System.out.println("┣━━━━>     ║━━━━   ║     <━━━━━┫");
+			System.out.println("┣━━━━━>    ╚═══╦═══╝      <━━━━┫");
+			System.out.println("┣━━━━>     ╔═══╩═══╗     <━━━━━┫");
+			System.out.println("┣━━━━━> ║  ║       ║  ║   <━━━━┫");
+			System.out.println("┣━━━━> ═╬══╣       ╠══╬═ <━━━━━┫");
+			System.out.println("┣━━━━━> ║  ║       ║  ║   <━━━━┫");
+			System.out.println("┣━━━━>     ╚═══╦═══╝     <━━━━━┫");
+			System.out.println("┣━━━━━>     ╔══╩══╗       <━━━━┫");
+			System.out.println("┣━━━━>      ║     ║      <━━━━━┫");
+			System.out.println("┣━━━━━>   ══╝     ╚══     <━━━━┫");
+			System.out.println("═════════════════════════════════════════════════");			
+			try{Thread.sleep(wait);}catch(InterruptedException e){System.out.println(e);}
+			
+			clrscr();
+			
+			System.out.println(" ");
+			System.out.println("           ╔══╗ ╔══╗ ╔═╦═╗╔══     ");
+			System.out.println("           ║    ║  ║ ║ ║ ║║    ");
+			System.out.println("           ║ ═╗ ╠══╣ ║ ║ ║╠══ ");
+			System.out.println("           ╚══╝ ║  ║ ║   ║╚══  ");
+			System.out.println("           ╔══╗      ╔══ ╔══╗   ");
+			System.out.println("           ║  ║ ║  ║ ║   ║  ║   ");
+			System.out.println("           ║  ║ ║  ║ ╠══ ╠══╝   "       );
+			System.out.println("           ╚══╝  ╲╱  ╚══ ║ ╲  ");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+			System.out.println("═════════════════════════════════════════════════");			
+			try{Thread.sleep(6000);}catch(InterruptedException e){System.out.println(e);}
+			
+			clrscr();
+		}
+	}
+	
+	static void clrscr()
+	{
+		System.out.print("\033[H\033[2J"); 
+	}
+}
+
+
